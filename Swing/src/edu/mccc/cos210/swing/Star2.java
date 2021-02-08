60a96edf27c20efc15cd12631021ed742adae56f
Nihar Gavva <nihargavva@gmail.com>
2021-02-08 16:39:27 -0500

    created a test file

---
+++ Swing/src/edu/mccc/cos210/swing/Star2.java
@@ -0,0 +1,96 @@
+package edu.mccc.cos210.swing;
+
+import java.awt.*;
+import java.awt.geom.*;
+import javax.swing.*;
+
+public class Star2
+{
+    public static void main(String[] args)
+    {
+        SwingUtilities.invokeLater(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                createAndShowGUI();
+            }
+        });
+    }
+
+    private static void createAndShowGUI()
+    {
+        JFrame f = new JFrame();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.getContentPane().add(new DrawStarShapePanel());
+        f.setSize(600, 600);
+        f.setLocationRelativeTo(null);
+        f.setVisible(true);
+    }
+
+}
+
+class DrawStarShapePanel extends JPanel
+{
+    @Override
+    protected void paintComponent(Graphics gr)
+    {
+        super.paintComponent(gr);
+        Graphics2D g = (Graphics2D) gr;
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, getWidth(), getHeight());
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+            RenderingHints.VALUE_ANTIALIAS_ON);
+
+        g.setColor(Color.BLACK);
+        g.draw(createDefaultStar(50, 200, 200));
+
+
+        
+
+
+    }
+
+    private static Shape createDefaultStar(double radius, double centerX,
+        double centerY)
+    {
+        return createStar(centerX, centerY, radius, radius * 2.63, 5,
+            Math.toRadians(-18));
+    }
+
+    private static Shape createStar(double centerX, double centerY,
+        double innerRadius, double outerRadius, int numRays,
+        double startAngleRad)
+    {
+        Path2D path = new Path2D.Double();
+        double deltaAngleRad = Math.PI / numRays;
+        for (int i = 0; i < numRays * 2; i++)
+        {
+            double angleRad = startAngleRad + i * deltaAngleRad;
+            double ca = Math.cos(angleRad);
+            double sa = Math.sin(angleRad);
+            double relX = ca;
+            double relY = sa;
+            if ((i & 1) == 0)
+            {
+                relX *= outerRadius;
+                relY *= outerRadius;
+            }
+            else
+            {
+                relX *= innerRadius;
+                relY *= innerRadius;
+            }
+            if (i == 0)
+            {
+                path.moveTo(centerX + relX, centerY + relY);
+            }
+            else
+            {
+                path.lineTo(centerX + relX, centerY + relY);
+            }
+        }
+        path.closePath();
+        return path;
+    }
+}
