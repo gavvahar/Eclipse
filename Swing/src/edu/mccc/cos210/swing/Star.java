6f369beec6ba17bdda942a1168d1f2cb72cd9b1c
Nihar Gavva <nihargavva@gmail.com>
2021-02-08 16:52:27 -0500

    drew the star and got it to rotate

--- Swing/src/edu/mccc/cos210/swing/Star.java
+++ Swing/src/edu/mccc/cos210/swing/Star.java
@@ -25,6 +25,8 @@ public class Star {
 		private static final long serialVersionUID = 1L;
 		private int FPS = 30;
 		private long lastTime = 0;
+		private double rotation1 = 0.0;
+		private double rotDelta1 = 1.0;
 
 		public MyJPanel() {
 			setBackground(Color.WHITE);
@@ -45,9 +47,62 @@ public class Star {
 			gat.scale(1.0, -1.0);
 			g2d.transform(gat);
 
+			//g2d.setColor(Color.BLACK);
+        	//g2d.draw(createDefaultStar(50, 200, 200));
+			var s2d = createDefaultStar(50, 200, 200);
+			AffineTransform at = new AffineTransform();
+			at.rotate(Math.toRadians(rotation1));
+			rotation1 += rotDelta1;
+			Shape s = at.createTransformedShape(s2d);
+			g2d.setPaint(new Color(128, 128, 0));
+			g2d.fill(s);
+			g2d.setPaint(Color.BLACK);
+			g2d.draw(s);
+
 			g2d.dispose();
 			Toolkit.getDefaultToolkit().sync();
 		}
 	}
+	private static Shape createDefaultStar(double radius, double centerX,
+        double centerY)
+    {
+        return createStar(centerX, centerY, radius, radius * 2.63, 5,
+            Math.toRadians(-18));
+    }
 
+    private static Shape createStar(double centerX, double centerY,
+        double innerRadius, double outerRadius, int numRays,
+        double startAngleRad)
+    {
+        Path2D path = new Path2D.Double();
+        double deltaAngleRad = Math.PI / numRays;
+        for (int i = 0; i < numRays * 2; i++)
+        {
+            double angleRad = startAngleRad + i * deltaAngleRad;
+            double ca = Math.cos(angleRad);
+            double sa = Math.sin(angleRad);
+            double relX = ca;
+            double relY = sa;
+            if ((i & 1) == 0)
+            {
+                relX *= outerRadius;
+                relY *= outerRadius;
+            }
+            else
+            {
+                relX *= innerRadius;
+                relY *= innerRadius;
+            }
+            if (i == 0)
+            {
+                path.moveTo(centerX + relX, centerY + relY);
+            }
+            else
+            {
+                path.lineTo(centerX + relX, centerY + relY);
+            }
+        }
+        path.closePath();
+        return path;
+    }
 }
