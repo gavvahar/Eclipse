29be43a40de674c5774e4a009fcf3d33cbb8128a
Nihar Gavva <nihargavva@gmail.com>
2021-02-13 21:20:02 -0500

    got the doubles to work

--- Words/src/edu/mccc/cos210/words/Doubles.java
+++ Words/src/edu/mccc/cos210/words/Doubles.java
@@ -10,7 +10,26 @@ public class Doubles
 	{
 		try (BufferedReader br = new BufferedReader(new FileReader("./data/pocket.dic")))
 		{
-			br.lines().forEach(System.out::println);
+			//br.lines().forEach(System.out::println);
+			br.lines().forEach(word -> {
+				char prevChar = word.charAt(0);
+				char prevPrevChar = ' ';
+				int doubleCount = 0;
+				for(int i = 1; i< word.length(); i++)
+				{
+					char nextChar = word.charAt(i);
+					if(nextChar == prevChar && nextChar != prevPrevChar)
+					{
+						doubleCount++;
+					}
+					prevPrevChar = prevChar;
+					prevChar = nextChar;
+				}
+				if(doubleCount > 0)
+				{
+					System.out.println(word + " " + doubleCount);
+				}
+			});
 		} catch (Exception ex)
 		{
 			System.err.println(ex);
