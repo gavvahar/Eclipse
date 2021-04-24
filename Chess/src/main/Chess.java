c28d48695999958314083e7f841cfc93aa57e6d3
gavvahar <nihargavva@gmail.com>
2021-04-24 12:59:23 -0400

    updated version

--- Chess/src/main/Chess.java
+++ Chess/src/main/Chess.java
@@ -6,7 +6,6 @@ import java.awt.geom.Ellipse2D;
 import java.beans.*;
 
 import javax.swing.*;
-import javax.swing.border.MatteBorder;
 
 public class Chess {
 	private static final int SIZE = 8;
@@ -15,7 +14,7 @@ public class Chess {
 	private Square startSpot = null;
 	private Square endSpot = null;
 	private JPanel jp = new JPanel();
-	private Piece[][] pieces = new Piece[SIZE][SIZE];
+	//private Piece[][] pieces = new Piece[SIZE][SIZE];
 		
 	public Chess() {
 		
@@ -24,7 +23,7 @@ public class Chess {
 		
 
 		jp.setLayout(new GridLayout(SIZE, SIZE));
-		jp.setBorder(BorderFactory.createLineBorder(Color.black, 35));
+		jp.setBorder(BorderFactory.createLineBorder(Color.black, 10));
 				
 		jp.setPreferredSize(new Dimension(800 / SIZE, 800 / SIZE));
 		//PropertyChangeListener pcl = this::propertyChange;
@@ -33,7 +32,8 @@ public class Chess {
 
 		for (int row = 0; row < SIZE; row++) {
 			for (int col = 0; col < SIZE; col++) {
-				Square square = new Square(row, col);
+				Piece piece = getInitialPieces(row,col);
+				Square square = new Square(row, col, piece);
 				jp.add(square);
 				this.squares[row][col] = square;
 				//spot.addPropertyChangeListener(pcl);
@@ -42,20 +42,82 @@ public class Chess {
 			}
 		}
 
+		JPanel leftPanel = getLeftPanel();
+		jf.add(leftPanel, BorderLayout.WEST);
+
+		JPanel rightPanel = new JPanel();
+		rightPanel.setBackground(Color.BLACK);
+		jf.add(rightPanel, BorderLayout.EAST);
+		
+
 		jf.add(jp, BorderLayout.CENTER);
 		jf.setSize(800, 800);
 		jf.setLocationRelativeTo(null);
 		jf.setResizable(false);
 		jf.setVisible(true);
 	}
-	public Piece getPiece(int row, int col)
-	{
-		return pieces[row][col];
+
+	private Piece getInitialPieces(int row, int col)	{
+		int pieceSize = 70;
+		String pawn = "\u265F";
+		Color color = Color.GREEN;
+		String strPiece = "";
+		
+		if (row == 0 )
+			strPiece = getPiece(row,col);	
+		else if (row == 1 ) {
+			strPiece = pawn;
+		} else if ( row == 6 ) {
+			color  = Color.RED;
+			strPiece = pawn;
+		} else if ( row == 7 ) {
+			color  = Color.RED;
+			strPiece = getPiece(row,col);	
+		}
+
+
+		return ( new Piece(color, strPiece));
 	}
-	public void setPiece(Piece piece, int row, int col)
-	{
-		pieces[row][col] = piece;
+	private String getPiece(int row, int col) {
+		String strPiece = "";
+		String queen = "\u265B";
+		String king = "\u265A";
+		String castle = "\u265C";
+		String knight = "\u265E";
+		String bishop = "\u265D";
+
+		if(col == 0 || col == 7) {
+			strPiece = castle;
+		} else if(col == 1 || col == 6)		{
+			strPiece =  knight;
+		} 	else if(col == 2 || col == 5) {
+			strPiece =  bishop;
+		}	else if(col == 4) 		{
+			strPiece =  king ;
+		} 	else if(col == 3) 		{
+			strPiece =  queen;
+		}
+	
+		return strPiece;
+
 	}
+	private JPanel getLeftPanel()	{
+		JPanel pbLayout = new JPanel(new GridBagLayout());
+		//GridLayout(int rows, int cols, int hgap, int vgap)
+		JPanel leftPanelNumbers = new JPanel(new GridLayout(8, 1, 0, 50));
+
+		for (int row = 7; row >= 0; row--) {
+			JPanel jb = new JPanel( String.valueOf(row) );
+			
+			leftPanelNumbers.add(jb);
+		}
+			
+		pbLayout.add(leftPanelNumbers);
+		pbLayout.setBackground(Color.BLACK);
+		leftPanelNumbers.setBackground(Color.BLACK);
+		return pbLayout;
+	}
+
 	public static void main(String... args) {
 		EventQueue.invokeLater(Chess::new);
 	}
--- Chess/src/main/Square.java
+++ Chess/src/main/Square.java
@@ -11,51 +11,25 @@ public class Square extends JPanel {
 		private static final long serialVersionUID = 1L;
 		private int row;
 		private int col;
-		private Color color;
-		public Square(int row, int col) {
+		private Color cellColor;
+		private Piece piece;
+
+		public Square(int row, int col, Piece piece) {
 			setLayout(null);
 			setEnabled(true);
 			this.row = row;
 			this.col = col;
+			this.piece = piece;
 						
 			//if((row!=0 && col!=0 && (row + col) % 2 == 0 )) {
 			if((row + col) % 2 == 0 ) {
-				this.color = Color.WHITE;
+				this.cellColor = Color.WHITE;
 			} else {
-				this.color = Color.BLACK;
+				this.cellColor = Color.BLACK;
 			}
 				
-			setBackground(this.color);
+			setBackground(this.cellColor);
 			setBorder(new MatteBorder(1, 1, 1, 1, new Color(200, 200, 224)));
-		}
-
-		private void setRow(Graphics2D g2d)		{
-			String queen = "\u265B";
-			String king = "\u265A";
-			String castle = "\u265C";
-			String knight = "\u265E";
-			String bishop = "\u265D";
-			
-			if(col == 0 || col == 7)
-			{
-				g2d.drawString(castle, 5, 70);
-			}
-			else if(col == 1 || col == 6)
-			{
-				g2d.drawString(knight, 5, 70);
-			}
-			else if(col == 2 || col == 5)
-			{
-				g2d.drawString(bishop, 5, 70);
-			}
-			else if(col == 4)
-			{
-				g2d.drawString(king, 5, 70);
-			}
-			else if(col == 3)
-			{
-				g2d.drawString(queen, 5, 70);
-			}
 		}
 
 		@Override 
@@ -68,38 +42,7 @@ public class Square extends JPanel {
 			//Piece piece = pieces[row][col];
 			g2d.setPaint(piece.color);
 			g2d.drawString(piece.pieceType, 5, 70);	
-			
-				
-			if(row == 0 && col < 8)
-			{
-				g2d.setFont(new Font("LucidaSans", Font.PLAIN, 80));
-				g2d.setPaint(color.GREEN);
-				setRow(g2d);
-			}
-			else if (row == 1 && col < 8) {
-				g2d.setPaint(color.GREEN);
-			g2d.setFont(new Font("LucidaSans", Font.PLAIN, 80));
-			g2d.drawString("\u265F", 5, 70);
-			} else if ( row == 6 && col < 8) {
-				g2d.setPaint(color.RED);
-				g2d.setFont(new Font("LucidaSans", Font.PLAIN, 80));
-				//g2d.setPaint(color.BLACK);
-				g2d.drawString("\u265F", 5, 70);	
-			} else if ( row == 7 && col < 8) {
-				//JLabel wKnight = new
-				g2d.setFont(new Font("LucidaSans", Font.PLAIN, 80));
-				g2d.setPaint(color.RED);
-				setRow(g2d);
-				//g2d.fill((Shape) color.BLUE);
-				//g2d.drawString("\u2658", 5, 70);
-			}
-			
-			//JLabel pawnLabel = new JLabel("\u265F");
-			//jp.add(pawnLabel);
-			
-			//Ellipse2D e2d = new Ellipse2D.Double(0.0, 0.0, getWidth() - 2.0, getHeight() - 2.0);
-			//g2d.setPaint(Color.BLUE);
-			//g2d.fill(e2d);
+							
 			g2d.dispose();
 		}
 }
